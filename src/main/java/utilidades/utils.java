f7a12328eef14377fa3b2da1c18f19de776fa536
ClaseJavaGetafe <Tardes@GET-04>
2025-10-24 20:35:42 +0200

    creo metodos: ganarEquipo, dadoDiez e invocaCriatura

--- src/main/java/utilidades/utils.java
+++ src/main/java/utilidades/utils.java
@@ -1,11 +1,33 @@
 package utilidades;
 
+import equipo.Equipamiento;
+import personajes.Personaje;
+
 public class utils {
 
-	//TODO
-	//Metodos
-	
-	//ganarEquipo();
-	//invocacion();
-	
+	// TODO
+	// Metodos
+
+	// ganarEquipo();
+
+	/**
+	 * @param ganaEquipo equipo que queremos añadir al personaje clase Equipamiento
+	 * @param person     personaje al que añadimos equipo, clase Personaje
+	 */
+	public static void ganarEquipo(Equipamiento ganaEquipo, Personaje person) {
+
+		person.getEquipo().add(ganaEquipo);
+		System.out.println("Has gando equipo: " + ganaEquipo);
+
+	}
+	// invocacion();
+
+	/**
+	 * @return devuelve int resultado tirada
+	 */
+	public static int dadoDiez() {
+		int tirada = (int) (Math.random() * 10 + 1);
+		return tirada;
+	}
+
 }
