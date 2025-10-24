7a9a2fefc5f849e3aec86170d6aba466f154d14f
ClaseJavaGetafe <Tardes@GET-04>
2025-10-24 20:35:58 +0200

    subo utils no terminado invoca

--- src/main/java/utilidades/utils.java
+++ src/main/java/utilidades/utils.java
@@ -1,5 +1,6 @@
 package utilidades;
 
+import criaturas.Criatura;
 import equipo.Equipamiento;
 import personajes.Personaje;
 
@@ -17,10 +18,23 @@ public class utils {
 	public static void ganarEquipo(Equipamiento ganaEquipo, Personaje person) {
 
 		person.getEquipo().add(ganaEquipo);
-		System.out.println("Has gando equipo: " + ganaEquipo);
+		System.out.println("Has gando equipo: " + ganaEquipo.toString());
 
 	}
 	// invocacion();
+	
+	public static void invocacionCompañeroCriatura (Criatura compi, Personaje person) {
+		
+	boolean	resultado = dadoDiez()>3;
+	if (resultado) {
+		person.getCriaturas().add(compi);
+		System.out.println("Hora tienes un compañero de viaje:" + compi.toString());
+	}else {
+		System.out.println("No estan pensado en lo que debes, al invocar la criatura se rie de ti y te ataca.");
+		person.set
+	}
+		
+	}
 
 	/**
 	 * @return devuelve int resultado tirada
