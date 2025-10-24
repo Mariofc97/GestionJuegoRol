09f3d412366b3cfae789437889eaf478bdd6c5fe
Mario Ferron <mario@email.com>
2025-10-24 20:09:33 +0200

    cambio personaje con lista de criaturas

--- src/main/java/personajes/Personaje.java
+++ src/main/java/personajes/Personaje.java
@@ -3,24 +3,53 @@ package personajes;
 import java.util.List;
 
 import clases.Raza;
+import criaturas.Criatura;
 import equipo.Equipamiento;
 
-public class Personaje extends Raza{
+public class Personaje extends Raza {
 
 	private List<Equipamiento> equipo;
-	
+	private List<Criatura> criaturas;
+	private String nombre;
+
 	public Personaje(String tipo, int fuerza, int inteligencia, int suerte) {
 		super(tipo, fuerza, inteligencia, suerte);
 		// TODO Auto-generated constructor stub
 	}
-	
-	
 
-	
-	
-	
+	public Personaje(String tipo, int fuerza, int inteligencia, int suerte, List<Equipamiento> equipo,
+			List<Criatura> criaturas, String nombre) {
+		super(tipo, fuerza, inteligencia, suerte);
+		this.equipo = equipo;
+		this.criaturas = criaturas;
+		this.nombre = nombre;
+	}
 
-	//PERSONAJE
+	public List<Equipamiento> getEquipo() {
+		return equipo;
+	}
+
+	public void setEquipo(List<Equipamiento> equipo) {
+		this.equipo = equipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public List<Criatura> getCriaturas() {
+		return criaturas;
+	}
+
+	public void setCriaturas(List<Criatura> criaturas) {
+		this.criaturas = criaturas;
+	}
+
+	// PERSONAJE
 	// public void ganarExperiencia(int experiencia);
 	// public void curar()
 	// public void usarPocion();
