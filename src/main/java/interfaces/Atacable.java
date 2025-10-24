adba6b40cdb7af035bbaec9bb1c99d27acd86b49
Mario Ferron <mario@email.com>
2025-10-24 20:48:32 +0200

    añadido de interfaces atacable y defendible

---
+++ src/main/java/interfaces/Atacable.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface Atacable {
+	
+	public void atacar();
+
+	
+}
--- src/main/java/personajes/Personaje.java
+++ src/main/java/personajes/Personaje.java
@@ -28,7 +28,7 @@ public class Personaje extends Raza {
 		this.criaturas = criaturas;
 		this.nombre = nombre;
 	}
-
+	
 	public Personaje(String tipo, int fuerza, int inteligencia, int suerte, List<Equipamiento> equipo,
 			List<Criatura> criaturas, String nombre, int experiencia, int puntosVida, int puntosAtaque) {
 		super(tipo, fuerza, inteligencia, suerte);
@@ -39,6 +39,8 @@ public class Personaje extends Raza {
 		this.puntosVida = puntosVida;
 		this.puntosAtaque = puntosAtaque;
 	}
+	
+	
 
 	public int getExperiencia() {
 		return experiencia;
@@ -87,25 +89,31 @@ public class Personaje extends Raza {
 	public void setCriaturas(List<Criatura> criaturas) {
 		this.criaturas = criaturas;
 	}
-
+	
 	public int ganarExperiencia(int experienciaAñadida) {
 		this.experiencia = experiencia + experienciaAñadida;
-
+		
 		System.out.println("El personaje " + nombre + " ha ganado " + experienciaAñadida + " puntos de experiencia");
 		System.out.println("Ahora mismo tiene " + experiencia + " de experiencia");
-
+		
 		return this.experiencia;
 	}
-
+	
 	public int usarPocion(Pocion pocion) {
-
+		
 		int puntosVidaPocion = pocion.getPuntosVida();
-
+		
 		this.puntosVida = puntosVida + puntosVidaPocion;
 		System.out.println("El personaje " + nombre + " se ha curado " + puntosVidaPocion + " puntos de vida gracias");
-
+		
 		return this.puntosVida;
 	}
+	
+	
+	
+	
+	
+	
 
 	// PERSONAJE
 	// public void ganarExperiencia(int experiencia);
