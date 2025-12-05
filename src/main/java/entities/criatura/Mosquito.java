f4b9ce60c6c0b2362afcc6d9447028c314171518
Isabel Eclipse <Tardes@GET-09>
2025-12-05 17:56:17 +0100

    mosquito

--- src/main/java/entities/criatura/Mosquito.java
+++ src/main/java/entities/criatura/Mosquito.java
@@ -4,7 +4,7 @@ public class Mosquito extends Criatura {
 
 	public Mosquito(String nombre, String alias, int nivel, int experiencia, int puntosVida, int puntosAtaque,
 			String tipoAtaque) {
-		super("Mosquito", alias, 1, 0, 20, 1, "picadura");
+		super("Mosquito", alias, 1, 0, 20, 1, "Picadura");
 	}
 
 	public Mosquito() {
