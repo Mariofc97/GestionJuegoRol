ecdb5e5400be6a3d70138e40a5033ead493f1466
Isabel Eclipse <Tardes@GET-09>
2025-12-05 18:03:42 +0100

    mosqui y rato

--- src/main/java/entities/criatura/Mosquito.java
+++ src/main/java/entities/criatura/Mosquito.java
@@ -4,7 +4,7 @@ public class Mosquito extends Criatura {
 
 	public Mosquito(String nombre, String alias, int nivel, int experiencia, int puntosVida, int puntosAtaque,
 			String tipoAtaque) {
-		super("Mosquito", alias, 1, 0, 20, 1, "Picadura");
+		super("Mosquito", alias, 1, 0, 20, 1, "PicaduraFatal");
 	}
 
 	public Mosquito() {
--- src/main/java/entities/criatura/Raton.java
+++ src/main/java/entities/criatura/Raton.java
@@ -4,7 +4,7 @@ public class Raton extends Criatura {
 
 	public Raton(String nombre, String alias, int nivel, int experiencia, int puntosVida, int puntosAtaque,
 			String tipoAtaque) {
-		super("Raton", alias, 1, 0, 20, 1, "mordisco");
+		super("Raton", alias, 1, 0, 20, 1, "MordiscoInfeccioso");
 
 	}
 
