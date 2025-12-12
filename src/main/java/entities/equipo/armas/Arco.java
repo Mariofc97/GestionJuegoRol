049491b1128d8f4256d29505982d231c448a2c1d
manuv <manuv@Manuel_VC>
2025-12-12 19:29:17 +0100

    Merge branch 'master' of https://github.com/Mariofc97/GestionJuegoRol.git

---
+++ src/main/java/entities/equipo/armas/Arco.java
@@ -0,0 +1,11 @@
+package entities.equipo.armas;
+
+public class Arco extends Armas{
+	
+	public Arco() {
+		super("Flecha", 8,9,95,8);
+		
+	}
+	
+
+}
---
+++ src/main/java/entities/equipo/armas/Cerbatana.java
@@ -0,0 +1,10 @@
+package entities.equipo.armas;
+
+public class Cerbatana extends Armas {
+
+	public Cerbatana(){
+		
+super("Dardo", 8,10,75,6);
+
+}
+}
