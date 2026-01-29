f5e62ad9f1861228f87608aa35c2017bf14f6da7
Isabel Eclipse <Tardes@GET-09>
2026-01-29 19:54:10 +0100

    empiezo episodio 5

--- src/main/java/entities/episodios/Episodio5JefeCueva.java
+++ src/main/java/entities/episodios/Episodio5JefeCueva.java
@@ -1,5 +1,6 @@
 package entities.episodios;
 
 public class Episodio5JefeCueva {
-
+//combate con el jefe de clan, dos resultados pierdes la mayoria de las vez pero se te reconoce como miembro valioso del clan, recuperas tu familia.....
+	//ganas el combate... en un estado de locura matas al jefe brutalmente... y te das cuenta que ha sido un error, todo el clan té mira, con ojos de asombro, miedo, rechazo, no consigues nada, es más lo pierdes todo, eres desterrado.
 }
